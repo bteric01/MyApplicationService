d6fdf193ec60acb8610dafc573849cba60b98614
bteric01 <bteric01@louisville.edu>
2015-10-26 00:03:49 -0400

    Updated activity_main.xml file
    Added new things into the file

--- app/src/main/java/myapplication/example/com/myapplicationservice/MainActivity.java
+++ app/src/main/java/myapplication/example/com/myapplicationservice/MainActivity.java
@@ -1,9 +1,11 @@
 package myapplication.example.com.myapplicationservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    //Method to start the Service
+    public void startService(View view){
+        startService(new Intent(getBaseContext(),MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 
     @Override
